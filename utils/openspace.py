50a2b9c60efb44747e68d0c6c07fab22710cf9bf
Husain <mohammedhussin1998@gmail.com>
2024-10-24 14:23:59 +0200

    Open Space intilization

--- utils/openspace.py
+++ utils/openspace.py
@@ -1,5 +1,49 @@
-import table
-class Openspace:
-    Tables
+from table import Table,Seat
+import random
+
+class Openespace:
+
     def __init__(self) -> None:
-        pass
+        self.tables=[None]*6
+        self.number_of_tables=6
+    
+        pass 
+
+
+
+    def organize(self,name:str)-> None:
+        
+        check_index= 0
+
+        while(True):
+
+            check_index+=1
+            if check_index==24:
+                print("All tables are occupied ! You can wait until you get an empty seat!")
+                return
+            
+            Random_table = random.randint(0, 5)
+
+            if self.tables[Random_table].has_free_spot():
+                seat_number=self.tables.assign_seat(name)
+                print (f"Assigned to table{ Random_table + 1 } on seat # {seat_number +1}")
+                return
+            
+            return
+        return
+    
+    
+    def display(self)->None:
+
+        for table in range(6):
+            print(f"on Table {table+1} set the folwoing people: /n ")
+            for seat in range(4):
+                print(f"{seat+1} - {self.tables[table].seats[seat].occupant }")
+   
+   
+   
+    def store (file:str)->None:
+        
+            
+
+
--- utils/table.py
+++ utils/table.py
@@ -1,43 +1,70 @@
 
-class Seat():
-    def __init__(self) -> None:
-        pass
-    
+import random
+
+class Seat:
+  
+    def __init__(self, free : bool , occupant : str) -> None:
+        self.free = free
+        self.occupant = occupant
+
+    def __str__(self)-> str:
+        
+        if self.free == False:
+            return f"The seat is for {self.occupant}"
+        else:
+            return "The seat is free "
+
+    def set_occupant(self, name: str)-> None:
+        if self.free == True:
+            self.occupant = name
+
+    def remove_occupant(self)-> str:
+        previous_occupant = self.occupant
+        self.occupant = ""
+        if self.free == False:
+            self.free == True
+        return previous_occupant
 
 
 
-class Table():
+
+
+
+
+class Table:
+
+
     def __init__(self, list_of_seats :Seat ) -> None:
-        self.seat_list= []
+        self.seats= [None]*4
         self.capacity = 4
 
 
     def has_free_spot(self)-> bool:
-        return self.capacity >  len(self.seat_list)
+        return self.capacity <  4
     
 
-    def assign_seat(self,name:str)-> None:
+    def assign_seat(self,name:str)-> int:
         New_comer = Seat(name)
-        if self.capacity >  len(self.seat_list):
-            self.seat_list.append(New_comer)
-        else:
-            print("No more capacit on this table")
-    
+        while(True):
+            Random_seat = random.randint(0, 3)
+            if 4 >  sum(x is not None for x in self.seats) and self.seats[Random_seat]==None :
+                self.seats[Random_seat]=New_comer
+                self.capacity-=1
+                return Random_seat
+
+            elif 4 >  sum(x is not None for x in self.seats) :
+                continue
+            else:
+                print("No more capacity on this table")
+                break
+        
     def left_capacity (self)-> int:
-        return self.capacity - len(self.seat_list)
+        return 4 - sum(x is not None for x in self.seats)
 
     
 
 
         
-
-
-
-
-
-
-
-
 
 
 
